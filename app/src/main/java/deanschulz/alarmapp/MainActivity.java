e585d57b31298ae3958c9d51d980582b953f36b2
Schulz, Dean <dpschu2@ilstu.edu>
2017-11-17 04:23:11 -0600

    Created basic layout to start out with.

--- app/src/main/java/deanschulz/alarmapp/MainActivity.java
+++ app/src/main/java/deanschulz/alarmapp/MainActivity.java
@@ -2,46 +2,171 @@ package deanschulz.alarmapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SectionDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.ncapdevi.fragnav.FragNavController;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnMenuTabClickListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private BottomBar mBottomBar;
+    private FragNavController fragNavController;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    //indices to fragments
+    private final int TAB_FIRST = FragNavController.TAB1;
+    private final int TAB_SECOND = FragNavController.TAB2;
+    private final int TAB_THIRD = FragNavController.TAB3;
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-
-                case R.id.navigation_home:
-                    Intent i = new Intent(MainActivity.this, MainActivity.class);
-                    startActivity(i);
-                    return true;
-                case R.id.navigation_dashboard:
-                    Intent i2 = new Intent(MainActivity.this, TimerActivity.class);
-                    startActivity(i2);
-                    return true;
-                case R.id.navigation_notifications:
-                    Intent i3 = new Intent(MainActivity.this, LocationActivity.class);
-                    startActivity(i3);
-                    return true;
-            }
-            return false;
-        }
-
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.main);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        //FragNav
+        //list of fragments
+        List<Fragment> fragments = new ArrayList<>(3);
+
+        //add fragments to list
+        fragments.add(AlarmFragment.newInstance(0));
+        fragments.add(TimerFragment.newInstance(0));
+        fragments.add(LocationFragment.newInstance(0));
+
+        //link fragments to container
+        fragNavController = new FragNavController(getSupportFragmentManager(),R.id.container,fragments);
+        //End of FragNav
+
+        //BottomBar menu
+        mBottomBar = BottomBar.attach(this, savedInstanceState);
+        mBottomBar.setItems(R.menu.bottombar_menu);
+        mBottomBar.setOnMenuTabClickListener(new OnMenuTabClickListener() {
+            @Override
+            public void onMenuTabSelected(@IdRes int menuItemId) {
+                //switch between tabs
+                switch (menuItemId) {
+                    case R.id.bottomBarItemOne:
+                        fragNavController.switchTab(TAB_FIRST);
+                        break;
+                    case R.id.bottomBarItemSecond:
+                        fragNavController.switchTab(TAB_SECOND);
+                        break;
+                    case R.id.bottomBarItemThird:
+                        fragNavController.switchTab(TAB_THIRD);
+                        break;
+                }
+            }
+
+            @Override
+            public void onMenuTabReSelected(@IdRes int menuItemId) {
+                if (menuItemId == R.id.bottomBarItemOne) {
+                    fragNavController.clearStack();
+                }
+            }
+        });
+        //End of BottomBar menu
+
+        //Navigation drawer
+        new DrawerBuilder().withActivity(this).build();
+
+        //primary items
+        PrimaryDrawerItem home = new PrimaryDrawerItem()
+                .withIdentifier(1)
+                .withName(R.string.app_list)
+                .withIcon(R.drawable.ic_home_black_24dp);
+
+
+        //Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(false);
+
+        new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withActionBarDrawerToggleAnimated(true)
+                .withTranslucentStatusBar(false)
+                .withFullscreen(true)
+                .withSavedInstance(savedInstanceState)
+                .addDrawerItems(
+                        home,
+                        new SectionDrawerItem().withName("fill this with alarms"),
+                        new DividerDrawerItem(),
+                        new SectionDrawerItem().withName("fill this with alarms"),
+                        new DividerDrawerItem()
+
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        if (drawerItem != null) {
+                            Intent intent = null;
+                            if (drawerItem.getIdentifier() == 1) {
+                                intent = new Intent(MainActivity.this, MainActivity.class);
+                            } else if (drawerItem.getIdentifier() == 2) {
+                                //intent = new Intent(MainActivity.this, Class.class);
+                            } else if (drawerItem.getIdentifier() == 3) {
+                                //intent = new Intent(MainActivity.this, Class.class);
+                            } else if (drawerItem.getIdentifier() == 11) {
+                                //intent = new Intent(MainActivity.this, Class.class);
+                            } else if (drawerItem.getIdentifier() == 12) {
+                                //intent = new Intent(MainActivity.this, Class.class);
+                            } else if (drawerItem.getIdentifier() == 13) {
+                                //intent = new Intent(MainActivity.this, Class.class);
+                            }
+                            if (intent != null) {
+                                MainActivity.this.startActivity(intent);
+                            }
+                        }
+
+                        return false;
+                    }
+                })
+                .build();
+        //End of Navigation drawer
+
+    }
+    @Override
+    public void onBackPressed() {
+        if (fragNavController.getCurrentStack().size() > 1) {
+            fragNavController.pop();
+        } else {
+            super.onBackPressed();
+        }
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // Necessary to restore the BottomBar's state, otherwise we would
+        // lose the current tab on orientation change.
+        mBottomBar.onSaveInstanceState(outState);
     }
 
 }
